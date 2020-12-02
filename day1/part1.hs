6677d7873457d6665432b4d7b5bae5fe28a4ec73
anhtumai <tu.mai@outlook.com>
2020-12-02 17:47:16 +0200

    complete day 2

---
+++ day1/part1.hs
@@ -0,0 +1,31 @@
+{-# LANGUAGE TemplateHaskell #-}
+
+import System.IO
+import Control.Monad
+import Data.List
+
+main = do
+        let list = []
+        handle <- openFile "day1_input.txt" ReadMode
+        contents <- hGetContents handle
+        let singlewords = words contents
+            list = f singlewords
+        let sortedList = sort list
+        let answer = getAnswer $ getPair 2020 sortedList
+        print answer
+        hClose handle
+
+f :: [String] -> [Int]
+f = map read
+
+getPair :: Int -> [Int] -> Maybe (Int, Int)
+getPair sum [] = Nothing
+getPair sum [x] = Nothing
+getPair sum (x:xs)
+    | x + last xs < sum = getPair sum xs
+    | x + last xs > sum = getPair sum (x:init xs)
+    | otherwise = Just (x, last xs)
+
+getAnswer :: Maybe (Int,Int) -> String
+getAnswer Nothing = "There are no pairs satisfying the condition"
+getAnswer (Just (x,y)) = "The answer is " ++ show (x*y)
